bc2b83d6b9123f3b388f8db0d9d11508ff303351
dtwogud <dtwogud@nate.com>
2021-09-08 21:44:27 +0900

    1강 완료

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,22 +1,56 @@
+import {createStore} from "redux";
+
+// const add = document.getElementById("add");
+// const minus = document.getElementById("minus");
+// const number = document.querySelector("span");
+
+// let count = 0;
+// number.innerText = count;
+// const updateTxt = () => {
+//   number.innerText = count;
+
+// }
+
+// const handleAdd = () => { 
+//   count += 1;
+// updateTxt();
+// }
+// const handleMinus = () => { 
+//   count -= 1;
+// updateTxt();
+// }
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-const updateTxt = () => {
-  number.innerText = count;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
+const countModifier = (count = 0, action) => {   //reducer 생성
+  switch(action.type){
+    case "ADD":
+      return count += 1;
+    case "MINUS":
+      return count -= 1;
+    default:
+      return count;
+  } 
 }
 
-const handleAdd = () => { 
-  count += 1;
-updateTxt();
-}
-const handleMinus = () => { 
-  count -= 1;
-updateTxt();
+const countStore = createStore(countModifier);   //store생성
+// countStore.dispatch({type : "ADD"})
+// countStore.dispatch({type : "MINUS"})
+// console.log(countStore.getState());
+
+const onChange = () => {
+  number.innerText = countStore.getState();
 }
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => {countStore.dispatch({type : ADD})})
+minus.addEventListener("click", () => {countStore.dispatch({type : MINUS})})
