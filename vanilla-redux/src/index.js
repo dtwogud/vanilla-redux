028c15996ce97d1e005d40bfca7925dee4318565
dtwogud <dtwogud@nate.com>
2021-09-15 14:22:18 +0900

    3.4 delete 완료

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,71 +1,12 @@
-import {createStore} from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./store";
 
-const form = document.querySelector("form")
-const input = document.querySelector("input")
-const ul = document.querySelector("ul")
-
-const ADD_TODO = "ADD_TODO";
-const DEL_TODO = "DEL_TODO";
-
-const addTodo = text => {
-  return {
-    type : ADD_TODO, text
-  }
-}
-
-const delTodo = id => {
-  return{
-    type : DEL_TODO, id
-  }
-}
-
-const reducer = (state=[], action) => {
-  switch(action.type){
-    case ADD_TODO:
-      const newTodo = {text:action.text, id:Date.now()}
-      return[newTodo, ...state]
-    case DEL_TODO:
-      const cleaned =  state.filter(toDo => toDo.id !== action.id)
-      return cleaned
-    default:
-      return state;
-  }
-}
-const store = createStore(reducer);
-
-store.subscribe(()=>{console.log(store.getState())})
-
-const onSubmit = event => {
-  event.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddTodo(toDo)
-}
-
-const dispatchAddTodo = text => {
-  store.dispatch(addTodo(text))
-}
-
-const disdpatchDelTodo = e => {
-  const id = parseInt(e.target.parentNode.id)
-  store.dispatch(delTodo(id))
-}
-
-const paintTodos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach(toDo => {
-    const li = document.createElement("li")
-    const btn = document.createElement("button")
-    btn.innerText = "Del"
-    btn.addEventListener("click", disdpatchDelTodo)
-    li.id = toDo.id
-    li.innerText = toDo.text
-    li.appendChild(btn)
-    ul.appendChild(li)
-  }
+ReactDOM.render(
+  <Provider store = {store}>
+    <App/>
+  </Provider>
+  ,document.getElementById("root")
   )
-}
-
-store.subscribe(paintTodos)
-form.addEventListener("submit",onSubmit)
---
+++ vanilla-redux/src/routes/Home.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import {connect} from "react-redux";
+import {actionCreators} from "../store"
+import ToDo from "../components/ToDo"
+
+function Home({toDos,addTodo}){
+  const [text, setText] = useState("")
+  function onChange(e){
+    setText(e.target.value)
+  }
+
+  function onSubmit(e){
+    e.preventDefault();
+    addTodo(text);
+    setText("")
+  }
+
+  return (
+  <>
+    <h1>To Do</h1>
+    <form onSubmit={onSubmit}>
+      <input type="text" value={text} onChange={onChange}></input>
+      <button>Add</button>
+    </form> 
+    <ul>
+      {toDos.map(toDo =>(
+      <ToDo {...toDo} key={toDo.id}/>
+      ))}
+      </ul>
+  </>
+  )
+}
+
+//mapStateToProps(다른이름 사용 가능은 하나 보통 mapStateToProps로 사용)
+//mapStateToProps(Redux state에서 온 state, component의 props(지금은 사용X))
+function mapStateToProps(state){
+  // console.log(state)
+  return {toDos : state}
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+    addTodo : (text) => dispatch(actionCreators.addTodo(text))
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps) (Home);
