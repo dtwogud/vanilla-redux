9780d51f7a3ea82c62be398a1fb1fa242fef1257
dtwogud <dtwogud@nate.com>
2021-09-27 22:07:53 +0900

    createReducer

--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,5 +1,5 @@
 import {createStore} from "redux";
-import {createAction} from "@reduxjs/toolkit";
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
 // const ADD = "ADD"
 // const DELETE = "DELETE"
@@ -21,19 +21,29 @@ const addTodo = createAction("ADD") //Action의 이름
 const delTodo = createAction("DELETE") //Action의 이름
 
 
-const reducer = (state = [], action) => {
-  switch(action.type) {
-    //ADD와 DELETE가 이미 지워졌으므로 각 함수의 type으로 설정
-    case addTodo.type:
-      // return[{text:action.payload, id:Date.now()},...state]
-      return[{text:action.payload, id:Date.now()},...state]
-    case delTodo.type:
-      // return state.filter(toDo => toDo.id !== action.payload)
-      return state.filter(toDo => toDo.id !== action.payload)
-    default:
-      return state;
-  }
-}
+// const reducer = (state = [], action) => {
+//   switch(action.type) {
+//     //ADD와 DELETE가 이미 지워졌으므로 각 함수의 type으로 설정
+//     case addTodo.type:
+//       // return[{text:action.text, id:Date.now()},...state]
+//       return[{text:action.payload, id:Date.now()},...state]
+//     case delTodo.type:
+//       // return state.filter(toDo => toDo.id !== action.payload)
+//       return state.filter(toDo => toDo.id !== action.payload)
+//     default:
+//       return state;
+//   }
+// }
+
+const reducer = createReducer([],{
+  [addTodo] : (state, action) => {
+    state.push({text:action.payload, id:Date.now()})
+  },
+  [delTodo] : (state, action) =>
+    state.filter(toDo => toDo.id !== action.payload)
+})
+
+// redux toolkit은 immer아래에서 작동하기에 mutate와 새로운 state return모두 사용 가능(return값은 무조건 새로운 state)
 
 const store = createStore(reducer)
 
