3e021158aeeb80c4336f8c45df01f9f64e9ce642
dtwogud <dtwogud@nate.com>
2021-09-27 16:03:29 +0900

    redux toolkit update-createAction

--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,28 +1,35 @@
 import {createStore} from "redux";
+import {createAction} from "@reduxjs/toolkit";
 
-const ADD = "ADD"
-const DELETE = "DELETE"
+// const ADD = "ADD"
+// const DELETE = "DELETE"
+// const addTodo = (text) => {
+//   return{
+//     type : ADD,
+//     text
+//   }
+// }
 
-const addTodo = (text) => {
-  return{
-    type : ADD,
-    text
-  }
-}
+// const delTodo = id =>{
+//   return{
+//     type : DELETE,
+//     id : parseInt(id)
+//   }
+// }
 
-const delTodo = id =>{
-  return{
-    type : DELETE,
-    id
-  }
-}
+const addTodo = createAction("ADD") //Action의 이름
+const delTodo = createAction("DELETE") //Action의 이름
+
 
 const reducer = (state = [], action) => {
   switch(action.type) {
-    case ADD:
-      return[{text:action.text, id:Date.now()},...state]
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id)
+    //ADD와 DELETE가 이미 지워졌으므로 각 함수의 type으로 설정
+    case addTodo.type:
+      // return[{text:action.payload, id:Date.now()},...state]
+      return[{text:action.payload, id:Date.now()},...state]
+    case delTodo.type:
+      // return state.filter(toDo => toDo.id !== action.payload)
+      return state.filter(toDo => toDo.id !== action.payload)
     default:
       return state;
   }
